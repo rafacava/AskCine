36b2e9199c0236605a4aeb66765b7b015b02508a
rafacava <rafacavagomes@gmail.com>
2017-06-12 07:27:32 -0300

    Adicionando a pasta assets do sistema

---
+++ askcine/assets/js/funcao.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_flat-red',
+    radioClass: 'iradio_flat-red'
+  });
+});
+
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+}
